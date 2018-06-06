d63c816e8df2c65ba5b38850e524c41b739185f0
Vladimir <vladimirrudt@yandex.ru>
2018-06-06 13:37:41 +0700

    теперь все нормально

---
+++ FuncionalPTD/FunctionalClasses/CASFileMaker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FuncionalPTD.FunctionalClasses;
+using FuncionalPTD.FunctionalInterfaces;
+using DomainPTD.DomainClasses;
+using Excel = Microsoft.Office.Interop.Excel;
+using DomainPTD.DomainInterfaces;
+
+namespace FuncionalPTD.FunctionalClasses
+{
+    public delegate List<Work> MakeFunction(IWorkFile workFileList);
+
+    public class CASFileMaker : IFileMaker
+    {
+        public CASFileMaker()
+        {
+            TypeFileList.Add("xlsx", ExcelMakeWorkList);
+        }
+
+        public Dictionary<string, MakeFunction> TypeFileList { get; set; }
+            = new Dictionary<string, MakeFunction>();
+
+        public IWorkFile MakeFile(List<IWorkFile> ExcelMakeWorkList)
+        {
+            List<List<Work>> allWorks = new List<List<Work>>();
+            foreach (var temp in workFileList)
+            {
+                MakeFunction function = TypeFileList[temp.Extension];
+                a
+            }
+        }
+
+        public List<Work> ExcelMakeWorkList(IWorkFile workFileList)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ PTDProject/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FuncionalPTD.FunctionalClasses;
+using DomainPTD.DomainClasses;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace PTDProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Excel.Application TempImportExcel = new Excel.Application(); ;
+            Excel.Workbook TempWoorkBook =
+            TempImportExcel.Application.Workbooks.Open(@"C:\Users\Андрей\Desktop\Ik22_Fakt_Vypolnenie_Sogl_Dogovora_Gp_Na_07_11_2017 (2).xlsx");
+            Excel.Worksheet TempWorkSheet = TempWoorkBook.Worksheets.get_Item(1);
+            TempImportExcel.DisplayAlerts = false;
+            CASExcelContrWorkInfoMaker Legion = new CASExcelContrWorkInfoMaker();
+            Work work = Legion.MakeInfoWork(TempImportExcel, 1);
+            Console.WriteLine(work.Title + ' ' + work.AllocMoney);
+            foreach (var temp in work.PeriodList) { Console.WriteLine(temp.Date + " " + temp.Money); }
+            Console.ReadKey();
+            TempWoorkBook.Close(false);
+            TempImportExcel.Quit();
+            TempImportExcel = null;
+            TempWoorkBook = null;
+            TempWorkSheet = null;
+            GC.Collect();
+        }
+    }
+}
